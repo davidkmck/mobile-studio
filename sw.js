afba0eb73c26f2c4d8a863ac2a1e0274618981fc
davidkmck <108087097+davidkmck@users.noreply.github.com>
2026-06-27 12:36:01 -0400

    Add initial service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,31 @@
+const CACHE_NAME = 'mobile-studio-v0';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/apps/multitrack/index.html',
+  '/apps/beat-maker/index.html',
+  '/icon.png',
+  '/icon-sml.png'
+  // Add all CSS/JS files for the sub-apps here
+];
+
+self.addEventListener('install', (event) => {
+  self.skipWaiting(); // Forces the waiting service worker to become the active one
+});
+/*
+// Install the service worker and cache basic files
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+*/
+
+// Intercept requests to serve from cache if offline
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
